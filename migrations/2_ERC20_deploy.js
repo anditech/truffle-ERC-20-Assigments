894ca69e37dd87bfe3217de3db0e029e3758e53e
anditech <71329092+anditech@users.noreply.github.com>
2021-05-31 22:49:56 -0400

    Update
    Deployed contract minting 100 tokens to accounts [0]
    Error handling

--- migrations/2_ERC20_deploy.js
+++ migrations/2_ERC20_deploy.js
@@ -1,8 +1,12 @@
 const ERC20 = artifacts.require("ERC20");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(ERC20,"Omnia", "OMNIA", 18).then(async (instance)=>{
-    await instance.mint(accounts[0], 100);
-      console.log("100 tokens minted to account zero");
+  deployer.deploy(ERC20,"Omnia", "OMNIA", 18).then(instance => {
+    instance.mint(accounts[0], 100).then(() => {
+      instance.balanceOf(accounts[0]).then(balance => {
+        console.log(`The address ${accounts[0]} has ${balance} tokens`);
+      }).catch(err => console.log(`*Address Error* ${err}`));
+    }).catch(err => console.log(`*Balance Error* ${err}`)); 
+      
   });
 };

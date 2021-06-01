08b0cbb1c8dca369f8f30aab0cc848932ecd1705
anditech <71329092+anditech@users.noreply.github.com>
2021-06-01 10:55:52 -0400

    Update
    Played with function minting

--- migrations/2_ERC20_deploy.js
+++ migrations/2_ERC20_deploy.js
@@ -2,7 +2,7 @@ const ERC20 = artifacts.require("ERC20");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(ERC20,"Omnia", "OMNIA", 18).then(instance => {
-    instance.mint(accounts[0], 100).then(() => {
+    instance.mint(accounts[0], 1000).then(() => {
       instance.balanceOf(accounts[0]).then(balance => {
         console.log(`The address ${accounts[0]} has ${balance} tokens`);
       }).catch(err => console.log(`*Address Error* ${err}`));
